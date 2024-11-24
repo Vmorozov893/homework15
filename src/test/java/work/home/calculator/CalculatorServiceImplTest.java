6cca3b2dd2f24061a7d68ce2ee4e121a8674280c
MorozovVladislavSergeevich <vsmorozov@edu.hse.ru>
2024-11-25 01:19:37 +0300

    тест с ошибкой

--- src/test/java/work/home/calculator/CalculatorServiceImplTest.java
+++ src/test/java/work/home/calculator/CalculatorServiceImplTest.java
@@ -52,6 +52,13 @@ public class CalculatorServiceImplTest{
 
     @Test
     void divideException() {
+        int num1=10;
+        int num2=0;
+
+        Assertions.assertThrows(
+                DivideByZeroException.class,
+                ()->calculatorServiceimpl.divide(num1,num2)
+        );
     }
 
 }
